8b9b610554aff6d62bd849616e1aa0c93fea346e
firdous alam <firdous.alam2058@gmail.com>
2024-08-18 11:56:45 +0530

    adding link to bind app

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
-
+import Link from "next/link";
 export default function Home() {
   return (
-    <div>Homepage</div>
+    <div>
+      <Link href="/performance">Performance</Link><br></br>
+      <Link href="/reliability">Reliability</Link><br></br>
+      <Link href="/scale">Scale</Link>
+    </div>
   );
 }

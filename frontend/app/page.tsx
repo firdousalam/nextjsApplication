abaabb9dd600d5b52be43fe7304fa04543f92b0c
firdous alam <firdous.alam2058@gmail.com>
2024-08-18 12:04:54 +0530

    Adding Link and creating nav

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,10 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Home() {
   return (
-    <div>
-      <Link href="/performance">Performance</Link><br></br>
-      <Link href="/reliability">Reliability</Link><br></br>
-      <Link href="/scale">Scale</Link>
-    </div>
+    <div>Homepage</div>
   );
 }
